415a74b12fefe37d86eebbab73a1d3f166d28d5c
george <georgeelliott93@gmail.com>
2021-02-15 20:05:22 +0000

    Added Base HttpClient
    - Added base http client just has a get method that is the only thing that should be needed.
    - Created Client Extensions Method to deserialize api response

---
+++ CoinGecko.Client/Extensions/ClientExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinGecko.Client.Extensions
+{
+    public static class ClientExtensions
+    {
+        public static async Task<T> GetJsonResult<T>(this HttpContent response) => JsonConvert.DeserializeObject<T>(await response.ReadAsStringAsync());
+    }
+}
