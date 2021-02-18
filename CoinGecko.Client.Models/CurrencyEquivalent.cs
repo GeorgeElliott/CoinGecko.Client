b9f79db7d84d6054034aa7247c9e66f2040cacbe
george <georgeelliott93@gmail.com>
2021-02-18 22:09:45 +0000

    Remove redundant files
    - Removed files that are no longer needed

--- CoinGecko.Client.Models/CurrencyEquivalent.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CoinGecko.Client.Models
-{
-    public class CurrencyEquivalent
-    {
-        public string Name { get; set; }
-        public double? Value { get; set; }
-    }
-}
--- CoinGecko.Client.Models/Response/SimplePriceResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace CoinGecko.Client.Models
-{
-    public class SimplePriceResponse : Dictionary<string, Dictionary<string, double?>>
-    { }
-}
